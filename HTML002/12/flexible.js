adbbd5bf1bd43a9f03ce971cca5e950a5fc24b8e
Mr54233 <1104879088@qq.com>
2022-07-19 15:08:54 +0800

    update 12 | 02 md flexablejs

---
+++ HTML002/12/flexible.js
@@ -0,0 +1,48 @@
+(function flexible (window, document) {
+  // 获取网页中的根元素.
+  var docEl = document.documentElement
+  // 物理像素比
+  var dpr = window.devicePixelRatio || 1
+
+  // 设置body标签的字体大小
+  function setBodyFontSize () {
+    if (document.body) {
+      document.body.style.fontSize = (12 * dpr) + 'px'
+    }
+    else {
+      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+    }
+  }
+  setBodyFontSize();
+
+  // 设置根元素的字体大小
+  function setRemUnit () {
+    var rem = docEl.clientWidth / 10
+    docEl.style.fontSize = rem + 'px'
+  }
+
+  setRemUnit()
+
+  // reset rem unit on page resize
+  window.addEventListener('resize', setRemUnit)
+  // pageShow 事件 == load 事件 (大多数时候等价 , 
+  // 火狐浏览器缓存原页面load事件不触发)
+  window.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      setRemUnit()
+    }
+  })
+
+  // detect 0.5px supports
+  if (dpr >= 2) {
+    var fakeBody = document.createElement('body')
+    var testElement = document.createElement('div')
+    testElement.style.border = '.5px solid transparent'
+    fakeBody.appendChild(testElement)
+    docEl.appendChild(fakeBody)
+    if (testElement.offsetHeight === 1) {
+      docEl.classList.add('hairlines')
+    }
+    docEl.removeChild(fakeBody)
+  }
+}(window, document))
