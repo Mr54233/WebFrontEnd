3b876fa7f9e49a07c31e6f1a7aa75e5ba4740717
Mr54233 <1104879088@qq.com>
2022-08-02 20:06:18 +0800

    update likede

--- HTML002/20-数据可视化/likede/js/index.js
+++ HTML002/20-数据可视化/likede/js/index.js
@@ -15,6 +15,10 @@
 			.siblings(".content")
 			.css("display", "none");
 	});
+	$(".marquee-view .marquee").each(function () {
+		var rows = $(this).children().clone();
+		$(this).append(rows);
+	});
 })();
 
 // 点位分布
@@ -177,7 +181,7 @@
 				},
 			},
 		},
-		// 修改线性渐变色方式 1
+		// 修改线性渐变色方式
 		color: new echarts.graphic.LinearGradient(
 			// (x1,y2) 点到点 (x2,y2) 之间进行渐变
 			0,
@@ -225,6 +229,20 @@
 	});
 })();
 
+// 订单切换模块
+(function () {
+	// 实现选项卡效果
+	$(".order .filter").on("click", "a", function () {
+		// 切换高亮显示
+		$(this).addClass("active").siblings("a").removeClass("active");
+
+		$(".order .data")
+			.eq($(this).index())
+			.css("display", "")
+			.siblings(".data")
+			.css("display", "none");
+	});
+})();
 
 // 销售额统计
 (function () {
@@ -383,4 +401,267 @@
 	window.addEventListener("resize", function () {
 		myChart.resize();
 	});
+})();
+
+// 渠道分布
+(function () {
+	const dataBJ = [[90, 19, 56, 11, 34]];
+
+	const lineStyle = {
+		color: "#fff",
+		width: 1,
+		opacity: 0.5,
+	};
+	//1.实例化对象
+	var myChart = echarts.init(document.querySelector(".radar"));
+	//2.指定配置项和数据
+	var option = {
+		tooltip: {
+			show: true,
+			// 控制提示框组件的显示位置
+			position: ["60%", "10%"],
+			backgroundColor: "rgba(0,0,0,.5)",
+			borderColor: "rgba(0,0,0,.5)",
+			textStyle: {
+				color: "#fff",
+				fontSize: 10,
+			},
+		},
+
+		radar: {
+			radius: "55%",
+			indicator: [
+				{ name: "机场", max: 100 },
+				{ name: "商场", max: 100 },
+				{ name: "火车站", max: 100 },
+				{ name: "汽车站", max: 100 },
+				{ name: "地铁", max: 100 },
+			],
+			shape: "circle",
+			splitNumber: 4,
+			name: {
+				textStyle: {
+					color: "#4c9bfd",
+				},
+			},
+
+			splitLine: {
+				lineStyle: {
+					color: "rgba(255, 255, 255, 0.5)",
+				},
+			},
+			splitArea: {
+				show: false,
+			},
+			axisLine: {
+				lineStyle: {
+					color: "rgba(255, 255, 255, 0.5)",
+				},
+			},
+		},
+		series: [
+			{
+				name: "Beijing",
+				type: "radar",
+				lineStyle: lineStyle,
+				data: dataBJ,
+				symbol: "circle",
+				symbolSize: 5,
+				itemStyle: {
+					color: "#fff",
+				},
+				areaStyle: {
+					color: "rgba(238, 197, 102, 0.6)",
+				},
+				// 在圆点上显示相关数据
+				label: {
+					show: true,
+					color: "#fff",
+					fontSize: 10,
+				},
+			},
+		],
+	};
+	//3.配置项和数据给我们的实例化对象
+	myChart.setOption(option);
+	//4.适配浏览器缩放
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	});
+})();
+
+// 销售进度
+(function () {
+	var myChart = echarts.init(document.querySelector(".gauge"));
+	var option = {
+		series: [
+			{
+				startAngle: 180,
+				name: "销售进度",
+				type: "pie",
+
+				radius: ["130%", "150%"],
+				center: ["50%", "80%"],
+
+				labelLine: {
+					show: false,
+				},
+				hoverOffset: 0,
+				data: [
+					{
+						value: 100,
+						itemStyle: {
+							// 颜色渐变#00c9e0->#005fc1
+							color: new echarts.graphic.LinearGradient(
+								// (x1,y2) 点到点 (x2,y2) 之间进行渐变
+								0,
+								0,
+								0,
+								1,
+								[
+									{ offset: 0, color: "#00c9e0" }, // 0 起始颜色
+									{ offset: 1, color: "#005fc1" }, // 1 结束颜色
+								]
+							),
+						},
+					},
+					{ value: 100, itemStyle: { color: "#12274d" } },
+					{ value: 200, itemStyle: { color: "transparent" } },
+				],
+			},
+		],
+	};
+
+	myChart.setOption(option);
+	window.addEventListener("resize", function () {
+		myChart.resize();
+	});
+})();
+
+// 右侧省份
+(function () {
+	var hotData = [
+		{
+			city: "北京", // 城市
+			sales: "25,179", // 销售额
+			flag: true, //  上升还是下降
+			brands: [
+				//  品牌种类数据
+				{ name: "可爱多", num: "9,086", flag: true },
+				{ name: "娃哈哈", num: "8,341", flag: true },
+				{ name: "喜之郎", num: "7,407", flag: false },
+				{ name: "八喜", num: "6,080", flag: false },
+				{ name: "小洋人", num: "6,724", flag: false },
+				{ name: "好多鱼", num: "2,170", flag: true },
+			],
+		},
+		{
+			city: "河北",
+			sales: "23,252",
+			flag: false,
+			brands: [
+				{ name: "可爱多", num: "3,457", flag: false },
+				{ name: "娃哈哈", num: "2,124", flag: true },
+				{ name: "喜之郎", num: "8,907", flag: false },
+				{ name: "八喜", num: "6,080", flag: true },
+				{ name: "小洋人", num: "1,724", flag: false },
+				{ name: "好多鱼", num: "1,170", flag: false },
+			],
+		},
+		{
+			city: "上海",
+			sales: "20,760",
+			flag: true,
+			brands: [
+				{ name: "可爱多", num: "2,345", flag: true },
+				{ name: "娃哈哈", num: "7,109", flag: true },
+				{ name: "喜之郎", num: "3,701", flag: false },
+				{ name: "八喜", num: "6,080", flag: false },
+				{ name: "小洋人", num: "2,724", flag: false },
+				{ name: "好多鱼", num: "2,998", flag: true },
+			],
+		},
+		{
+			city: "江苏",
+			sales: "23,252",
+			flag: false,
+			brands: [
+				{ name: "可爱多", num: "2,156", flag: false },
+				{ name: "娃哈哈", num: "2,456", flag: true },
+				{ name: "喜之郎", num: "9,737", flag: true },
+				{ name: "八喜", num: "2,080", flag: true },
+				{ name: "小洋人", num: "8,724", flag: true },
+				{ name: "好多鱼", num: "1,770", flag: false },
+			],
+		},
+		{
+			city: "山东",
+			sales: "20,760",
+			flag: true,
+			brands: [
+				{ name: "可爱多", num: "9,567", flag: true },
+				{ name: "娃哈哈", num: "2,345", flag: false },
+				{ name: "喜之郎", num: "9,037", flag: false },
+				{ name: "八喜", num: "1,080", flag: true },
+				{ name: "小洋人", num: "4,724", flag: false },
+				{ name: "好多鱼", num: "9,999", flag: true },
+			],
+		},
+	];
+	var supHTML = "";
+	$.each(hotData, function (index, item) {
+		supHTML += `<li>
+		<span>${item.city}</span>
+		<span>${item.sales} <s class=${item.flag ? "icon-up" : "icon-down"}></s></span>
+	</li>`;
+	});
+	$(".sup").html(supHTML);
+
+	$(".province .sup").on("mouseenter", "li", function () {
+		index = $(this).index();
+
+		render($(this));
+	});
+
+	function render(that) {
+		//鼠标经过当前li要高亮
+		that.addClass("active").siblings().removeClass();
+		var subHTML = "";
+		$.each(hotData[that.index()].brands, function (index, item) {
+			subHTML += `<li><span>${item.name}</span><span>${
+				item.num
+			} <s class= ${
+				item.flag ? "icon-up" : "icon-down"
+			}></s></span></li>`;
+			$(".sub").html(subHTML);
+		});
+	}
+	// 所有的LI
+	var lis = $(".province .sup li");
+	// 第一个默认激活
+	lis.eq(0).mouseenter();
+	var index = 0;
+	var timer = setInterval(function () {
+		index++;
+		if (index >= 5) index = 0;
+		//lis.eq(index).mouseenter();
+		render(lis.eq(index));
+	}, 2000);
+
+	$(".province .sup").hover(
+		function () {
+			clearInterval(timer);
+		},
+		function () {
+			clearInterval(timer);
+
+			timer = setInterval(function () {
+				index++;
+				if (index >= 5) index = 0;
+				//lis.eq(index).mouseenter();
+
+				render(lis.eq(index));
+			}, 2000);
+		}
+	);
 })();
