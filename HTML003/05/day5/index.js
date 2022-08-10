f609b6555937a6122d2ef5b920be9ff779a610a5
Mr54233 <1104879088@qq.com>
2022-08-10 10:00:18 +0800

    update 003 05

---
+++ HTML003/05/day5/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+// 设置模板引擎
+app.engine("art", require("express-art-template"));
+
+// 设置模板文件所在的文件夹
+// app.set('views','pagers')
+app.set("views", path.join(__dirname, "pagers"));
+
+// 设置模板文件的默认扩展名为 art ,使用模板文件时 可以省略扩展名
+app.set("view engine", "art");
+
+// 挂在路由
+app.get("/", (req, res) => {
+	res.render("index", { name: "Lisi" });
+});
+
+app.get("/var", (req, res) => {
+	res.render("var", {
+		value: "welcome you ",
+		a: true,
+		b: 100,
+		c: 3000,
+		employee: {
+			name: "小亮",
+			salary: 8000,
+		},
+		span: '<a href="#"> 百度 </a>',
+	});
+});
+
+app.get("/if", (req, res) => {
+	res.render("if", {
+		pager: {
+			pageIndex: 3,
+			pages: 3,
+		},
+	});
+});
+
+app.get("/list", (req, res) => {
+	res.render("list", {
+		stus: [
+			{
+				no: 1,
+				name: "张三",
+				address: "江西南昌",
+			},
+			{
+				no: 2,
+				name: "李四",
+				address: "江西赣州",
+			},
+		],
+	});
+});
+
+// 启动服务器
+app.listen(port, () =>
+	console.log(`Example app listening on port http://127.0.0.1:${port}`)
+);
