569a7f4175cce6cd98443dfd9767293c6211bc7f
Mr54233 <1104879088@qq.com>
2022-08-10 15:17:12 +0800

    update 003 05  SMS3

--- HTML003/05/SMS3/index.js
+++ HTML003/05/SMS3/index.js
@@ -1,21 +1,28 @@
 const express = require("express");
-const path = require("path");
+const stuRoute = require('./routers/stu')
+
 const app = express();
 
 // 挂载静态资源文件
 app.use(express.static('public'))
+// 挂载中间件
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 
 // 设置模板引擎
 app.engine("art", require("express-art-template"));
 
 // 设置模板文件所在的文件夹
 // app.set('views','pagers')
-app.set("views", path.join(__dirname, "views"));
+// 默认为views
+// app.set("views", path.join(__dirname, "views"));
 
 // 设置模板文件的默认扩展名为 art ,使用模板文件时 可以省略扩展名
-app.set("view engine", "art");
+// 默认为art
+// app.set("view engine", "art");
 
 // 挂载路由
+app.use('/stu',stuRoute)
 
 app.listen(3000, () => {
 	console.log(`Example app listening on port http://127.0.0.1:3000`);
--- HTML003/05/SMS3/mymodules/data.js
+++ HTML003/05/SMS3/mymodules/data.js
@@ -1,9 +1,31 @@
-// 专门负责读写文件
-const path = require('path')
-const fs = require('fs')
+// 专门负责读写data.json文件 的模块
 
-const app = express()
+const path = require("path");
+const fs = require("fs");
+var fileName = path.join(__dirname, "../data.json");
 
-app.listen(3000,()=>{
-    console.log("http://127.0.0.1:3000")
-})
+// 读取文件内容 , 转换成数组返回
+function getAll(callback) {
+	fs.readFile(fileName, "utf-8", (err, result) => {
+		stus = JSON.parse(result);
+
+		// 读取的数据由函数的调用者传入回调函数来处理
+		callback(stus);
+	});
+}
+
+// 写入文件
+function writeData(student, callback) {
+	// arr 就是读取文件得到的原来的学生数组
+	getAll((arr) => {
+		arr.push(student);
+		fs.writeFile(fileName, JSON.stringify(arr), (err) => {
+			callback();
+		});
+	});
+}
+
+module.exports = {
+	getAll,
+	writeData,
+};
