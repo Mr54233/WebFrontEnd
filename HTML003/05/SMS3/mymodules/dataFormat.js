6802d0fbb9b0d62f25e982e1ebf53ec30e701415
Mr54233 <1104879088@qq.com>
2022-08-10 15:58:30 +0800

    update 003 05 SMS3

--- HTML003/05/SMS3/mymodules/dataFormat.js
+++ HTML003/05/SMS3/mymodules/dataFormat.js
@@ -1,3 +1,4 @@
+// 格式化传入的学生数据 ， 返回可视化数据
 const sex = ["男", "女"];
 const departs = ["前端与移动开发", "Java", "人工智能", "大数据", "物联网"];
 const hobby = ["编码", "打篮球", "睡觉"];
@@ -5,12 +6,15 @@ const hobby = ["编码", "打篮球", "睡觉"];
 function format(stu) {
 	stu.sex = sex[stu.sex];
 	stu.depart = departs[stu.depart];
+
+	// 遍历出复选框选中的所有爱好
 	for (var i = 0; i < stu.hobby.length; i++) {
 		stu.hobby[i] = hobby[stu.hobby[i]];
 	}
+	stu.hobby = String(stu.hobby);
 	return stu;
 }
 
 module.exports = {
-    format
-}
+	format,
+};
--- HTML003/05/SMS3/routers/stu.js
+++ HTML003/05/SMS3/routers/stu.js
@@ -1,9 +1,12 @@
+// 学生管理路由模块
 const express = require("express");
 const { getAll, writeData } = require("../mymodules/data");
 const df = require("../mymodules/dataFormat");
 
+// 创建路由
 const route = express.Router();
 
+// 挂载路由
 route.get("/list", (req, res) => {
 	getAll((result) => {
 		res.render("stu/list.art", { stus: result });
@@ -20,4 +23,9 @@ route.post("/add", (req, res) => {
 	});
 });
 
+route.get('/delete',(req,res)=>{
+    
+})
+
+// 导出路由
 module.exports = route;
