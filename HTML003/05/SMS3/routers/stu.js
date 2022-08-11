a46a7a9e47272735bf8061af9b5c839d0793c638
Mr54233 <1104879088@qq.com>
2022-08-11 08:07:33 +0800

    update 003 05

--- HTML003/05/SMS3/routers/stu.js
+++ HTML003/05/SMS3/routers/stu.js
@@ -1,8 +1,8 @@
 // 学生管理路由模块
 const express = require("express");
-const { getAll, writeData,deleteData } = require("../mymodules/data");
+const { getAll, writeData, deleteData } = require("../mymodules/data");
 const df = require("../mymodules/dataFormat");
-const Pager = require('../mymodules/pager')
+const Pager = require("../mymodules/pager");
 
 // 创建路由
 const route = express.Router();
@@ -12,7 +12,11 @@ route.get("/list", (req, res) => {
 	getAll((result) => {
 		res.render("stu/list.art", { stus: result });
 	});
-	req.query.page
+	console.log(req.query.page);
+	var pager = new Pager(1, 5, 9);
+	console.log(pager);
+
+	res.render("/stu/list.art", { pager });
 });
 
 route.get("/add", (req, res) => {
@@ -32,19 +36,19 @@ route.get("/delete", (req, res) => {
 				result.splice(i, 1);
 			}
 		}
-		deleteData(result)
+		deleteData(result);
 		res.redirect("/stu/list");
 		res.render("stu/list.art", { stus: result });
 	});
 });
 
-var i = 0
-getAll((result)=>{
-	i = result.length
-	console.log('i',i)
-})
-var pager = new Pager(1,5,i)
-
+// var i = 0;
+// getAll((result) => {
+// 	i = result.length;
+// 	console.log("i", i);
+// });
+// var pager = new Pager(1, 5, i);
+// console.log(pager);
 
 // 导出路由
 module.exports = route;
