5d46fae5ec0ef6faa88095210318bdd043b44e38
Mr54233 <1104879088@qq.com>
2022-09-01 09:57:00 +0800

    update 004 08

---
+++ HTML004/08/08plus/src/index.js
@@ -0,0 +1,7 @@
+// 1. 导入jQuery模块
+import $ from "jquery";
+
+$(function () {
+    $('#myul li:odd').css('backgroundColor','yellow')
+    $('#myul li:even').css('backgroundColor','')
+});
---
+++ HTML004/08/08plus/webpack.config.js
@@ -0,0 +1,8 @@
+modules.exports = {
+	// npm i webpack webpack-cli -D
+	// mode 用来指定构建模式
+	// 编译模式 : development 开发模式 , production 生产模式
+	// 在 production 模式下 , webpack执行编译 , 压缩和混淆
+	// 在 development 模式下 , webpack执行编译
+	mode: "development",
+};
