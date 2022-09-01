1fbf5dfd59803c342d6e01047d12a45e2b31c92b
Mr54233 <1104879088@qq.com>
2022-09-01 11:13:39 +0800

    update 004 08plus

--- HTML004/08/08plus/src/index.js
+++ HTML004/08/08plus/src/index.js
@@ -1,7 +1,13 @@
 // 1. 导入jQuery模块
 import $ from "jquery";
 
+// 导入css
+import './css/index.css'
+
+// 导入less
+import './css/my.less'
+
 $(function () {
-    $('#myul li:odd').css('backgroundColor','red')
+    $('#myul li:odd').css('backgroundColor','orange')
     $('#myul li:even').css('backgroundColor','green')
 });
--- HTML004/08/08plus/webpack.config.js
+++ HTML004/08/08plus/webpack.config.js
@@ -18,5 +18,11 @@ module.exports = {
 		path: path.join(__dirname, "./dist"),
 		filename: "bundle.js",
 	},
-    plugins:[htmlPlugin]
+    plugins:[htmlPlugin],
+    module:{
+        rules:[
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']}
+        ]
+    }
 };
