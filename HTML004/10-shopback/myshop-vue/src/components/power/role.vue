d9b667f6a7722213987cf44e2a9d4eee7756570c
Mr54233 <1104879088@qq.com>
2022-09-07 15:50:28 +0800

    update 004 10

--- HTML004/10-shopback/myshop-vue/src/components/power/role.vue
+++ HTML004/10-shopback/myshop-vue/src/components/power/role.vue
@@ -10,7 +10,7 @@
 		</el-breadcrumb>
 
 		<el-card class="box-card">
-			<!-- 搜索区域 -->
+			<!-- 按钮区域 -->
 			<el-row :gutter="20">
 				<el-col :span="4">
 					<el-button type="primary" @click="addDialogVisible = true"
@@ -91,11 +91,15 @@
 						</el-row>
 					</template>
 				</el-table-column>
+
 				<el-table-column type="index" width="50"></el-table-column>
+
 				<el-table-column prop="roleName" label="角色名称" width="180">
 				</el-table-column>
+
 				<el-table-column prop="roleDesc" label="角色描述" width="360">
 				</el-table-column>
+
 				<el-table-column label="操作">
 					<template slot-scope="scope">
 						<!-- {{scope.row}} -->
@@ -118,12 +122,26 @@
 							type="warning"
 							icon="el-icon-s-tools"
 							size="small"
+							@click="showSetRightDialog(scope.row)"
 							>分配权限</el-button
 						>
 					</template>
 				</el-table-column>
 			</el-table>
 		</el-card>
+
+		<!-- 分配权限对话框 -->
+		<el-dialog title="分配权限" :visible.sync="setRightVisible" width="50%">
+			<!-- 显示权限的树形控件 -->
+			<el-tree :data="rightList" :props="defaultProps" default-expand-all  show-checkbox :default-checked-keys="defaultKeys" node-key="id"></el-tree>
+
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="setRightVisible = false">取 消</el-button>
+				<el-button type="primary" @click="setRightVisible = false"
+					>确 定</el-button
+				>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 
@@ -132,6 +150,19 @@ export default {
 	data() {
 		return {
 			roleList: [],
+			// 控制分配权限对话框的显示和隐藏的
+			setRightVisible: false,
+			// 所有权限数据的数组
+			rightList: [],
+			// 控制树形控件显示数据的来源的格式
+			defaultProps: {
+				// 权限属性在哪个属性中
+				children: "children",
+				// 需要显示的数据的属性名
+				label: "authName",
+			},
+			// 树节点默认选中的id节点数组
+			defaultKeys:[101]
 		};
 	},
 	async created() {
@@ -143,6 +174,21 @@ export default {
 			this.$message.success(res.meta.msg);
 			this.roleList = res.data;
 		}
+	},
+	methods: {
+		async showSetRightDialog(role) {
+			// 获取所有的权限数据
+			const { data: res } = await this.$http.get("rights/tree");
+			if (res.meta.status !== 200) {
+				this.$message.error(res.meta.msg);
+			} else {
+				this.$message.success(res.meta.msg);
+				this.rightList = res.data;
+				console.log(this.roleList);
+			}
+			// 显示对话框
+			this.setRightVisible = true;
+		},
 	},
 };
 </script>
