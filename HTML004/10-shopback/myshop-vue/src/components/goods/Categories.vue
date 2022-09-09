71f93f6c19a182d6d432cae09abd3b6d586b6d62
Mr54233 <1104879088@qq.com>
2022-09-09 15:18:11 +0800

    update 004 10

--- HTML004/10-shopback/myshop-vue/src/components/goods/Categories.vue
+++ HTML004/10-shopback/myshop-vue/src/components/goods/Categories.vue
@@ -51,14 +51,14 @@
 						type="primary"
 						icon="el-icon-edit"
 						size="small"
-						@click="showUser(scope.row)"
+						@click="showEdit(scope.row)"
 						>编辑</el-button
 					>
 					<el-button
 						type="danger"
 						icon="el-icon-delete"
 						size="small"
-						@click="open(scope.row.id)"
+						@click="open(scope.row)"
 						>删除</el-button
 					>
 				</template>
@@ -95,12 +95,12 @@
 				</el-form-item>
 				<el-form-item label="父分类">
 					<el-cascader
-						v-model="addForm.cat_pid"
+						v-model="selectedCateIds"
 						:options="parentCateList"
 						:props="parentCateProps"
 						@change="handleChange"
-                        clearable
-                        change-on-select
+						clearable
+						change-on-select
 					></el-cascader>
 				</el-form-item>
 			</el-form>
@@ -108,6 +108,35 @@
 			<span slot="footer" class="dialog-footer">
 				<el-button @click="addCateVisible = false">取 消</el-button>
 				<el-button type="primary" @click="addCate">确 定</el-button>
+			</span>
+		</el-dialog>
+
+		<!-- 编辑类别的对话框 -->
+		<el-dialog
+			title="编辑类别"
+			:visible.sync="editCateVisible"
+			width="50%"
+			@close="handleEditClose"
+		>
+			<!-- 对话框的主要内容 -->
+			<el-form
+				ref="editForm"
+				:model="editForm"
+				label-width="80px"
+				:rules="editFormRules"
+			>
+				<el-form-item label="分类名称" prop="cat_name">
+					<el-input v-model="editForm.cat_name"></el-input>
+				</el-form-item>
+			</el-form>
+			<!-- 脚部两个按钮 -->
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="editCateVisible = false">取 消</el-button>
+				<el-button
+					type="primary"
+					@click="editCatesInfo(editForm.cat_id)"
+					>确 定</el-button
+				>
 			</span>
 		</el-dialog>
 	</div>
@@ -162,6 +191,7 @@ export default {
 			addForm: {
 				cat_name: "",
 				cat_pid: 0,
+				cat_level: 0,
 			},
 			// 添加分类表单验证规则
 			addFormRules: {
@@ -173,17 +203,34 @@ export default {
 					},
 				],
 			},
-            // 父分类列表
-            parentCateList:[],
-            // 父分类级联选择器的配置对象
-            parentCateProps:{
-                expandTrigger:"hover",
-                label:'cat_name',
-                value:'cat_id',
-                children:'children'
-                // checkStrictly:'true'
-            }
-
+			// 父分类列表
+			parentCateList: [],
+			// 选择的类型id数组
+			selectedCateIds: [],
+			// 父分类级联选择器的配置对象
+			parentCateProps: {
+				expandTrigger: "hover",
+				label: "cat_name",
+				value: "cat_id",
+				children: "children",
+				// checkStrictly:'true'
+			},
+            // 控制编辑分类对话框的显示和隐藏
+			editCateVisible: false,
+			// 添加分类表单
+			editForm: {
+				cat_name: "",
+			},
+			// 添加分类表单验证规则
+			editFormRules: {
+				cat_name: [
+					{
+						required: true,
+						message: "请输入分类名称",
+						trigger: "blur",
+					},
+				],
+			},
 		};
 	},
 	created() {
@@ -213,20 +260,119 @@ export default {
 			this.queryInfo.pagenum = newPage;
 			this.getCategoryList();
 		},
-        // 显示添加分类
-        async showAddCateDialog(){
-            // 获取父分类数据
-            const {data:res} = await this.$http.get(`categories`,{params:{type:2}})
-            if (res.meta.status !== 200) {
+		// 显示添加分类
+		async showAddCateDialog() {
+			// 获取父分类数据
+			const { data: res } = await this.$http.get(`categories`, {
+				params: { type: 2 },
+			});
+			if (res.meta.status !== 200) {
 				return this.$message.error(res.meta.msg);
 			}
-            this.parentCateList = res.data
-            // 打开对话框
-            this.addCateVisible = true
+			this.parentCateList = res.data;
+			// 打开对话框
+			this.addCateVisible = true;
+		},
+		// 关闭添加分类对话框
+		handleAddClose() {
+			this.$refs.addForm.resetFields();
+			this.addCateVisible = false;
+		},
+		// 添加分类确认
+		async addCate() {
+			// console.log(this.addForm);
+			const { data: res } = await this.$http.post(
+				"categories",
+				this.addForm
+			);
+			if (res.meta.status !== 201) {
+				return this.$message.error(res.meta.msg);
+			}
+
+			this.$message.success(res.meta.msg);
+			this.addCateVisible = false;
+			this.getCategoryList();
+		},
+
+		// 选择分类
+		handleChange() {
+			// 将选择中的父类型的id设置到addfrom 中的cate_pid
+			// 还需要设置cate_level层级
+			// [] [1] [1,3]
+			if (this.selectedCateIds.length == 0) {
+				// 没有选择父类别 , 表示添加的就是一级类别
+				this.addForm.cat_level = 0;
+				this.addForm.cat_pid = 0;
+			} else {
+				this.addForm.cat_level = this.selectedCateIds.length;
+				this.addForm.cat_pid =
+					this.selectedCateIds[this.selectedCateIds.length - 1];
+			}
+		},
+
+		// 打开编辑对话框
+		async showEdit(cates) {
+            console.log(cates);
+            this.editCateVisible = true
+
+            const { data:res} = await this.$http.get(`categories/${cates.cat_id}`) 
+            
+            this.editForm = res.data
         },
-        handleAddClose(){},
-        addCate(){},
-        handleChange(){}
+
+		// 打开确认删除框
+		open(cates) {
+			// console.log(cates);
+			this.$confirm(`此操作将永久删除该<span style="color:red;font-weight:bold;"> ${cates.cat_name} </span>分类, 是否继续?`, "提示", {
+				confirmButtonText: "确定",
+				cancelButtonText: "取消",
+				dangerouslyUseHTMLString: true,
+				type: "warning",
+			})
+				.then(async () => {
+					const { data: res } = await this.$http.delete(
+						`categories/${cates.cat_id}`
+					);
+					if (res.meta.status !== 200) {
+						this.$message.error(res.meta.msg);
+					} else {
+						this.$message.success(res.meta.msg);
+						this.getCategoryList();
+					}
+				})
+				.catch(() => {
+					this.$message({
+						type: "info",
+						message: "那就不删了奥",
+					});
+				});
+		},
+
+        // 打开编辑对话框
+        editCatesInfo(id){
+            // console.log(id);
+            this.$refs.editForm.validate(async (result)=>{
+                if (result) {
+					const { data: res } = await this.$http.put(
+						`categories/${id}`,
+						this.editForm
+					);
+					// console.log(res);
+					if (res.meta.status !== 200) {
+						this.$message.error(res.meta.msg);
+					} else {
+						this.$message.success(res.meta.msg);
+						this.getCategoryList();
+						this.editCateVisible = false;
+					}
+				}
+            })
+        },
+        // 关闭前编辑对话框
+        handleEditClose(){
+			this.$refs.editForm.resetFields();
+			this.editCateVisible = false;
+		},
 	},
 };
 </script>
