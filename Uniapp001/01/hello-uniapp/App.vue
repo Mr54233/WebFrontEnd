475f99304f9b66c1a6a158742380522ce7c119cc
Mr54233 <1104879088@qq.com>
2022-09-20 16:32:02 +0800

    update uniapp 01

--- Uniapp001/01/hello-uniapp/App.vue
+++ Uniapp001/01/hello-uniapp/App.vue
@@ -14,4 +14,10 @@
 
 <style>
 	/*每个页面公共css */
+	/* 导入字体图标 */
+	@import url("./static/font_pk5tgtvhtyr/iconfont.css");
+	/* 全局样式 */
+	.box1{
+		background-color: pink;
+	}
 </style>
---
+++ Uniapp001/01/hello-uniapp/pages/uni-style/uni-style.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view>
+			样式的学习
+		</view>
+		<view class="box1">
+			测试文字
+		</view>
+		<view class="iconfont icon-tupian">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	@import url("./a.css");
+.box1{
+	width: 350rpx;
+	height: 375rpx;
+	/* background-color: blue; */
+	font-size: 30rpx;
+	color: white;
+}
+
+</style>
