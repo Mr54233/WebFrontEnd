e800ff8666f36a0e9b54ab3ec0440aca6e11cdb0
Mr54233 <1104879088@qq.com>
2023-05-04 10:06:33 +0800

    update 04 React

--- HTML005/04/my-app/src/index.js
+++ HTML005/04/my-app/src/index.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// 2. 创建React元素
-const title = React.createElement('h1',null,'hello world')
+// 2. 使用jsx创建React元素
+const title = <h1>hello world <span>这是 span</span></h1>
 
 // 3. 渲染react元素
 ReactDOM.render(title,document.getElementById('root'))
