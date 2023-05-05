31f2f9f6f74ea9a0ed57ff76c82a2ee494015705
Mr54233 <1104879088@qq.com>
2023-05-05 10:47:32 +0800

    update 04 jsx

--- HTML005/04/my-app/src/index.js
+++ HTML005/04/my-app/src/index.js
@@ -1,9 +1,13 @@
 // 1. 导入react
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // 2. 使用jsx创建React元素
-const title = <h1>hello world <span>这是 span</span></h1>
+const title = (
+	<h1>
+		hello world <span>这是 span</span>
+	</h1>
+);
 
 // 3. 渲染react元素
-ReactDOM.render(title,document.getElementById('root'))
+ReactDOM.render(title, document.getElementById("root"));
