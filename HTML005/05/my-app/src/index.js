c524b6e5d91d1d76b02bb2e5e8b9ab72e908ae47
Mr54233 <1104879088@qq.com>
2023-05-09 20:08:09 +0800

    update React

--- HTML005/05/my-app/src/index.js
+++ HTML005/05/my-app/src/index.js
@@ -6,11 +6,19 @@ import ReactDOM from "react-dom";
 //
 const name = "Tom";
 const age = 19;
+const sayhi = () => 'hi';
+
+const h1 = <h1>我是jsx</h1>;
 const dv = (
 	<div>
 		<h1>
 			你好我叫：{name},年龄：{age}
+			<h1>{1}</h1>
+			<h1>{"a"}</h1>
+			<h1>{1+1}</h1>
+			<h1>{sayhi()}</h1>
 		</h1>
+		嵌入jsx：{h1}
 	</div>
 );
 
