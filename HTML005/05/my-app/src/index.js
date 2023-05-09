9ab63a9dd46849b22a269810ca261620bbf5f293
Mr54233 <1104879088@qq.com>
2023-05-09 09:30:12 +0800

    update react age

--- HTML005/05/my-app/src/index.js
+++ HTML005/05/my-app/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// 
+//
 // 嵌入js表达式
-// 
+//
 const name = "Tom";
+const age = 19;
 const dv = (
 	<div>
-		<h1>你好我叫：{name}</h1>
+		<h1>
+			你好我叫：{name},年龄：{age}
+		</h1>
 	</div>
 );
 
-ReactDOM.render(dv, document.getElementById('root'));
+ReactDOM.render(dv, document.getElementById("root"));
