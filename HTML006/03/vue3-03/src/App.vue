4a126afd9dec7db21f2738fabfe7572274d63aa2
Mr54233 <1104879088@qq.com>
2023-08-24 15:29:50 +0800

    update 生命周期，父子组件

---
+++ HTML006/03/vue3-03/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { reactive, ref, watchEffect, watch,onMounted } from 'vue';
+import SonCom from './views/sonCom.vue'
+</script>
+
+<template>
+  <div>
+    <h2>父组件App</h2>
+    <SonCom></SonCom>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ HTML006/03/vue3-03/src/views/sonCom.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { reactive, ref, watchEffect, watch,onMounted } from 'vue';
+</script>
+
+<template>
+<div class="son">
+    <h3>子组件Son</h3>
+</div>
+</template>
+
+<style scoped>
+.son{
+    background-color: red;
+}
+</style>
