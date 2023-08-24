35ae3f481cea69dc7cf0cc8799e6bec258fbe387
Mr54233 <1104879088@qq.com>
2023-08-24 10:58:17 +0800

    update  watch函数

--- HTML006/02/vue3-02/src/App.vue
+++ HTML006/02/vue3-02/src/App.vue
@@ -36,7 +36,7 @@ const logMessage = () => {
 <!-- ------ -->
 
 
-<!-- <script setup>
+<!-- <script setup> // 响应式数据
 // 1. 导入函数
 // import {reactive}  from "vue"
 
@@ -68,7 +68,9 @@ const setCount = ()=>{
   </div>
 </template> -->
 
-<script setup>
+<!-- ------------------- -->
+
+<!-- <script setup>  // 计算属性
 // 原始响应式数组
 import {ref} from "vue"
 const list = ref([1,2,3,4,5,6,7,8])
@@ -92,5 +94,96 @@ setTimeout(() => {
   </div>
   <div>
     {{ computedList }}
+  </div>
+</template> -->
+
+<!-- -------------------------- -->
+
+<!-- <script setup> // 监听属性
+import {ref,watch} from "vue"
+const count = ref(0)
+const setCount = ()=>{
+  count.value++
+}
+
+//   watch监听单个数据源
+//  ref对象不需要加.value
+watch(count,(newVal,oldVal)=>{
+  console.log('count发生了变化',newVal,oldVal)
+})
+
+
+</script>
+
+
+<template>
+  <div>
+    <button @click="setCount">+{{ count }}</button>
+  </div>
+</template> -->
+
+<!-- ------------ -->
+
+<!-- <script setup> // 监听多个数据源
+import {watch,ref} from 'vue'
+const count = ref(0)
+const name = ref("张三")
+
+const changeName = ()=>{
+  name.value = "李四"
+}
+const changeCount = ()=>{
+  count.value++
+}
+
+// 监听多个数据
+watch([count,name],
+      ([newCount,newName],[oldCount,oldName])=>{
+  console.log("新值"+newCount,newName,"老值"+oldCount,oldName)
+})
+</script>
+
+<template>
+  <div>
+    <button @click="changeCount">修改count--{{ count }}</button>
+  </div>
+  <div>
+    <button @click="changeName">修改name--{{ name }}</button>
+  </div>
+</template> -->
+
+<!-- ------------  -->
+
+<!-- <script setup>  // 立即执行
+  // 1. 导入函数
+  import {ref,watch} from "vue"
+  const count = ref(0)
+  const setCount = ()=>{
+    count.value++
+  }
+  // watch立即执行
+  watch(count,(newVal,oldVal)=>{
+    console.log('count发生了变化',newVal,oldVal)
+  },{
+    immediate:true
+  })
+</script>
+
+
+<template>
+  <div>
+    <button @click="setCount">+{{ count }}</button>
+  </div>
+</template> -->
+
+<!-- -------- -->
+
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    
   </div>
 </template>
